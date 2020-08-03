32483f1595980ae13d6730b76153327854ecf72d
Chris Ashley <cashley2k@outlook.com>
2020-08-03 12:43:31 +0100

    Add files via upload

---
+++ wake-pc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Limit Log Size =========================================================
+
+if [ -f /root/scripts/logs/wake-pc.log ]; then
+  tail -n 600 /root/scripts/logs/wake-pc.log > /root/scripts/logs/wake-pc.tmp
+  mv /root/scripts/logs/wake-pc.tmp /root/scripts/logs/wake-pc.log
+
+else
+   touch /root/cron-scripts/logs/wake-pc.log
+fi
+
+# ========================================================================
+
+now=$(date)
+
+if ether-wake 04:D9:F5:04:9B:AF ; then
+  echo "[$now] PC has awoken" >> /root/scripts/logs/wake-pc.log
+else
+  echo "[$now] PC still alseep" >> /root/scripts/logs/wake-pc.log
+fi
+
